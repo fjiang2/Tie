912bc860523240b918f98f2fa1733fe8173df4ee
Frank Jiang <Frank Jiang@localhost>
2014-01-16 18:53:39 -0600

    Define Enum as integer or string

--- Tie.Helper/Helper.cs
+++ Tie.Helper/Helper.cs
@@ -14,6 +14,49 @@ namespace Tie.Helper
         }
 
 
+        public static void DefineEnumAsInteger()
+        {
+            Valizer.Register<Enum>(
+                   host =>
+                   {
+                       return new VAL(Convert.ToInt32(host));
+                   },
+                   val =>
+                   {
+                       return (Enum)Enum.ToObject(typeof(Enum), val.Intcon);
+                   }
+               );
+        }
+
+        public static void DefineEnumAsString()
+        {
+
+            Valizer.Register<Enum>(
+              host =>
+              {
+                  return new VAL("\"" + host.ToString() + "\"");
+              },
+              (host, type, val) =>
+              {
+                  string s = val.Str;
+                  s = s.Substring(1, s.Length - 2);
+                  string[] L = s.Split(new char[] { ',' });
+                  if (L.Length == 1)
+                      return (Enum)Enum.Parse(type, s);
+                  else
+                  {
+                      int E = 0;
+                      for (int i = 0; i < L.Length; i++)
+                      {
+                          E += Convert.ToInt32(Enum.Parse(type, L[i].Trim()));
+                      }
+                      return (Enum)Enum.ToObject(typeof(Enum), E);
+                  }
+              }
+            );
+        }
+
+
     
     }
 }
