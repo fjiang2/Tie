06bc2c1b982a057dd26a8d7895f5a98b612d1a47
fjiang2 <fjiang2@hotmail.com>
2015-03-31 09:23:06 -0500

    add AsNumerable, ToDictionary, ToVAL on Tie.Helper

--- Tie.Helper/Extension.cs
+++ Tie.Helper/Extension.cs
@@ -16,6 +16,7 @@
 //--------------------------------------------------------------------------------------------------//
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
@@ -26,6 +27,156 @@ namespace Tie.Helper
 {
     public static class Extension
     {
+
+
+        /// <summary>
+        /// item which is not typeof(T) is ignored
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> AsEnumerable<T>(this VAL val)
+        {
+            List<T> list = new List<T>();
+            
+            foreach (var obj in val)
+            {
+                if (typeof(T) == typeof(VAL))
+                    list.Add((T)(object)obj);
+                else if(obj.HostValue is T)
+                    list.Add((T)obj.HostValue);
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// item which type is not T1, T2 is ignored
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public static IDictionary<T1, T2> ToDictionary<T1, T2>(this VAL val)
+        {
+            Dictionary<T1, T2> dict = new Dictionary<T1, T2>();
+
+            foreach (VAL L in val)
+            {
+                if (!L.IsList)
+                    continue;
+
+                if (L.Count >= 2)
+                {
+                    object x1 = typeof(T1) == typeof(VAL) ? L[0] : L[0].HostValue;
+                    object x2 = typeof(T2) == typeof(VAL) ? L[1] : L[1].HostValue;
+
+                    if (x1 is T1 && x2 is T2)
+                        dict.Add((T1)x1, (T2)x2);
+                }
+            }
+
+            return dict;
+        }
+
+        /// <summary>
+        /// return val when val is not null
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="val"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static T IsNull<T>(this VAL val, T defaultValue)
+        {
+            if (val.IsNull)
+                return defaultValue;
+
+            if (val.HostValue is T)
+                return (T)val.HostValue;
+
+             return default(T);
+        }
+
+        /// <summary>
+        /// return val when val is undefined
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="val"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static T Undefined<T>(this VAL val, T defaultValue)
+        {
+            if (val.Undefined)
+                return defaultValue;
+
+            if (val.HostValue is T)
+                return (T)val.HostValue;
+
+            return default(T);
+        }
+
+        public static T ToObject<T>(this VAL val, T defaultValue)
+        {
+            if (typeof(T) == typeof(VAL))
+                return (T)(object)val;
+
+            if (val.HostValue is T)
+                return (T)val.HostValue;
+
+            return defaultValue;
+        }
+
+
+        /// <summary>
+        /// Convert source into VAL, it is deep conversion except typeof(string)
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static VAL ToVAL(this IEnumerable source)
+        {
+            VAL L = VAL.Array();
+            
+            foreach (var obj in source)
+            {
+                if (obj is IEnumerable && !(obj is string))
+                    L.Add(ToVAL((IEnumerable)obj));
+                else
+                    L.Add(VAL.Boxing(obj));
+            }
+
+            return L;
+        }
+
+
+        /// <summary>
+        /// Convert Dictionary to associative array
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static VAL ToVAL<T1,T2>(this IDictionary<T1,T2> source)
+        {
+            VAL L = VAL.Array();
+
+            foreach (var kvp in source)
+            {
+                VAL L1 = VAL.Array();
+                L1.Add(VAL.Boxing(kvp.Key));
+                L1.Add(VAL.Boxing(kvp.Value));
+                
+                L.Add(L1);
+            }
+
+            return L;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public static VAL ToVAL(this object obj)
         {
             Contract.Ensures(obj != null);
--- tests/Tie.Helper.Tests/ExtensionTest.cs
+++ tests/Tie.Helper.Tests/ExtensionTest.cs
@@ -10,22 +10,90 @@ namespace Tie.Helper.Tests
 {
     public class ExtensionTest
     {
+        static string __Address;
+        static VAL __val;
+
+        static ExtensionTest()
+        {
+            // Arrange
+            string code = "{ Name : '1000 SH 6', City : 'Sugar Land', State : 'TX', Zip : '77578'}";
+            __val = Script.Evaluate(code);
+            __Address = __val.ToString();
+        }
+
         [Fact]
         public void TestToVAL()
         {
             //Arrange
             var address = new { Name="1000 SH 6", City = "Sugar Land", State = "TX", Zip = "77578"};
 
-            // Arrange
-            string code = "{ Name : '1000 SH 6', City : 'Sugar Land', State : 'TX', Zip : '77578'}";
-
             // Act
-            var val1 = address.ToVAL();
-            var val2 = Script.Evaluate(code);
+            var val = address.ToVAL();
+      
 
             // Assert
-            Assert.Equal(val1.ToString(), val2.ToString());
+            Assert.Equal(val.ToString(), __Address);
         }
 
+        [Fact]
+        public void DictinaryToVAL()
+        {
+            //Arrange
+            var dict = new Dictionary<string, string>();
+            dict.Add("Name", "1000 SH 6");
+            dict.Add("City",  "Sugar Land");
+            dict.Add("State", "TX");
+            dict.Add("Zip", "77578");
+
+            // Act
+            var val = dict.ToVAL();
+            
+            // Assert
+            Assert.Equal(val.ToString(), __Address);
+        }
+
+        [Fact]
+        public void EnumerableToVAL()
+        {
+            //Arrange
+            var list = new List<string[]>();
+            list.Add(new string[] {"Name", "1000 SH 6"});
+            list.Add(new string[] {"City", "Sugar Land"});
+            list.Add(new string[] {"State", "TX"});
+            list.Add(new string[] {"Zip", "77578"});
+
+            // Act
+            var val = list.ToVAL();
+
+            // Assert
+            Assert.Equal(val.ToString(), __Address);
+        }
+
+
+        [Fact]
+        public void ToDictionary()
+        {
+            //Arrange
+            IDictionary<string, string> dict = __val.ToDictionary<string, string>();
+
+            // Act
+            var state = dict["State"];
+
+            // Assert
+            Assert.Equal(state, "TX");
+        }
+
+        [Fact]
+        public void AsNumerable()
+        {
+            //Arrange
+            var list = __val.AsEnumerable<VAL>();
+
+            // Act
+            var zip = list.Last().AsEnumerable<string>();
+
+            // Assert
+            Assert.Equal(zip.Last(), "77578");
+        }
     }
 }
