d9faf84376058a958a9a484134c06a3b7d6e7857
Frank Jiang <fjiang2@hotmail.com>
2015-05-06 12:43:46 -0500

    Revert "merged VAL.cs"
    This reverts commit 96916b8371f02aef9282411e959e111026a58f30.

--- tests/Tie.UnitTest/SubclassTest.cs
+++ tests/Tie.UnitTest/SubclassTest.cs
@@ -12,20 +12,6 @@ namespace UnitTest
         public static void main()
         {
 
-
-            Valizer.Register<Form>(
-            delegate(Form wform)
-            {
-                return new VAL(typeof(Form).FullName);
-            },
-            delegate(Form wform, Type type, VAL val)
-            {
-                return new Form();
-            }
-         );
-
-
-
             string code = @"
 MyForm1 = class(text)
 {
